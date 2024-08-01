c2a1b2bab38c0254d728aa39755498ab5524478a
AyushGautam5 <153561102+AyushGautam5@users.noreply.github.com>
2024-08-01 23:27:38 +0530

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -99,12 +99,3 @@ public:
     }
 };
 
-/**
- * Your MyLinkedList object will be instantiated and called as such:
- * MyLinkedList* obj = new MyLinkedList();
- * int param_1 = obj->get(index);
- * obj->addAtHead(val);
- * obj->addAtTail(val);
- * obj->addAtIndex(index,val);
- * obj->deleteAtIndex(index);
- */
